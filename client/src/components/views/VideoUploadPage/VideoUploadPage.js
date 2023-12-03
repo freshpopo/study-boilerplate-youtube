3b3ea7eb18415b566e3cf55aa157a41c64416cfe
popo <freshposong@gmail.com>
2023-12-03 23:05:32 +0900

    VideoUploadPage 추가

---
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Typography, Button, Form, message, Input, Icon} from "antd";
+import Dropzone from "react-dropzone";
+
+const { Title } = Typography;
+const { TextArea } = Input;
+function VideoUploadPage () {
+  return (
+    <div style={{ maxWidth: '700px', margin: '2rem auto'}}>
+      <div style={{ textAlign: 'center', marginBottom: '2rem'}}>
+        <Title level={2}>Upload Video</Title>
+      </div>
+      <Form onSubmit>
+        <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+          {/* TODO : Dropzone 버전 다운 후 재시도 */}
+          {/* Drop Zone */}
+          {/*<Dropzone*/}
+          {/*  onDrop*/}
+          {/*  multiple*/}
+          {/*>*/}
+          {/*  maxSize{({ getRootProps, getInputProps}) => (*/}
+          {/*    <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', alignItems: 'center', justifyContent: 'center' }} {...getRootProps()}>*/}
+          {/*      <input {...getInputProps()}/>*/}
+          {/*      <Icon type="plus" style={{ fontSize: '3rem' }}></Icon>*/}
+          {/*    </div>*/}
+          {/*)}*/}
+          {/*</Dropzone>*/}
+          {/* Thumbnail */}
+          <div>
+            <img src="" alt=""/>
+          </div>
+        </div>
+        <br/>
+        <br/>
+        <Input
+          onChnage
+          value
+        ></Input>
+        <br/>
+        <br/>
+        <label>Description</label>
+        <TextArea
+          onChange
+          value
+        ></TextArea>
+        <br/>
+        <br/>
+        <select
+          onChange
+        >
+          <option key="" value=""></option>
+        </select>
+        <br/>
+        <br/>
+        <select
+          onChange
+        >
+          <option key="" value=""></option>
+        </select>
+        <br/>
+        <br/>
+        <Button type="primary" size="large" onClick>Submit</Button>
+      </Form>
+    </div>
+  )
+}
+
+export default VideoUploadPage
