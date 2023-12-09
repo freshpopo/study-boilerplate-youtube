3856e84faf0ccde649501d949e353e87c5383f8f
popo <freshposong@gmail.com>
2023-12-09 23:28:59 +0900

    썸네일 자동 생성

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -24,6 +24,9 @@ function VideoUploadPage () {
   const [Description, setDescription] = useState('');
   const [Private, setPrivate] = useState(0);
   const [Category, setCategory] = useState(0);
+  const [FilePath, setFilePath] = useState('')
+  const [Duration, setDuration] = useState('')
+  const [ThumbnailPath, setThumbnailPath] = useState('')
 
   const onTitleChange = (e) => {
     setVideoTitle(e.currentTarget.value);
@@ -47,7 +50,24 @@ function VideoUploadPage () {
     Axios.post('/api/video/uploadfiles', formData, config)
       .then(response => {
         if (response.data.success) {
-          console.log(response.data);
+          console.log(response.data)
+
+          let variable = {
+            url : response.data.url,
+            fileName: response.data.fileName
+          }
+
+          setFilePath(response.data.url);
+
+          Axios.post('/api/video/thumbnail', variable)
+            .then(response => {
+              if (response.data.success) {
+                setDuration(response.data.fileDuration);
+                setThumbnailPath(response.data.url);
+              } else {
+                alert('썸네일 생성에 실패했습니다.')
+              }
+            })
         } else {
           alert('비디오 업로드를 실패했습니다.')
         }
@@ -74,10 +94,12 @@ function VideoUploadPage () {
               </div>
           )}
           </Dropzone>
-           Thumbnail
-          <div>
-            <img src="" alt=""/>
-          </div>
+           {/*Thumbnail*/}
+          {ThumbnailPath &&
+            <div>
+              <img src={`http://localhost:5001/${ThumbnailPath}`} alt="thumbnail"/>
+            </div>
+          }
         </div>
         <br/>
         <br/>
