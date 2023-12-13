c4e8c4abca0f13bb3343488bbf5ad64c30ac0274
popo <freshposong@gmail.com>
2023-12-13 23:43:26 +0900

    구독기능 마무리

--- client/src/components/views/VideoDetailPage/Sections/Subscribe.js
+++ client/src/components/views/VideoDetailPage/Sections/Subscribe.js
@@ -31,10 +31,44 @@ function Subscribe (props) {
       })
   }, []);
 
+  const onSubscribe = () => {
+    let subscribedVariable = {
+      userTo: props.userTo,
+      userFrom: localStorage.userId,
+    };
+    // 이미 구독 중이라면
+    if (Subscribed) {
+      Axios.post('/api/subscribe/unSubscribed', subscribedVariable)
+        .then(response => {
+          if (response.data.success) {
+            setSubscribeNumber(SubscribeNumber - 1);
+            setSubscribed(!Subscribed);
+
+          } else {
+            alert('구독 취소 하는데 실패했습니다.')
+          }
+        })
+
+    // 구독중이 아니라면
+    } else {
+      Axios.post('/api/subscribe/subscribe', subscribedVariable)
+        .then(response => {
+          if (response.data.success) {
+            setSubscribeNumber(SubscribeNumber + 1);
+            setSubscribed(!Subscribed);
+          } else {
+            alert('구독 하는데 실패했습니다.')
+          }
+        })
+
+    }
+  }
+
   return (
     <div>
       <button
-        style={{ backgroundColor: `${ Subscribed ? '#CC0000' : '#AAAAAA'}`, color: 'white', padding: '10px 16px', borderRadius: '4px', fontWeight: '700', fontSize: '1rem', textTransform: 'uppercase' }}
+        style={{ backgroundColor: `${ Subscribed ? '#AAAAAA' : '#CC0000'}`, color: 'white', padding: '10px 16px', borderRadius: '4px', fontWeight: '700', fontSize: '1rem', textTransform: 'uppercase' }}
+        onClick={onSubscribe}
       >
         {SubscribeNumber} {Subscribed ? 'Subscribed' : 'Subscribe'}
       </button>
